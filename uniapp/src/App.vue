f9787d8951a7073eb08c3769960fd555fee8ddde
linyi <jingshuihuayue@qq.com>
2026-05-23 20:29:31 +0800

    feat: 添加UniApp应用入口与页面配置

---
+++ uniapp/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onLaunch } from '@dcloudio/uni-app'
+
+onLaunch(() => {
+  console.log('App Launch')
+})
+</script>
+
+<style>
+page {
+  background-color: #16213e;
+}
+</style>
